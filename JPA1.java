77c721fa03712329b4569cde65e31ea538d533b3
by0611 <156165417+by0611@users.noreply.github.com>
2024-03-12 09:54:55 +0800

    Add files via upload

---
+++ JPA1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class JPA1 {
+    static Scanner scn = new Scanner(System.in);
+    public static void main(String[] args) {
+        test();
+        test();
+    }
+    public static void test() {
+        System.out.println("Input: ");
+        int score = scn.nextInt();
+        if(score>=60){
+            System.out.println("You pass!");}
+        System.out.println("End");
+    }
+}
