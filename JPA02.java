a842dff9ef4a7089c2712507de553dc584c49406
by0611 <156165417+by0611@users.noreply.github.com>
2024-03-17 11:33:44 +0800

    Update JPA02.java

--- JPA02.java
+++ JPA02.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class JPA02 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);//宣告scn為輸入清單
-        for(int i = 0;i<=1;i++){
+        for(int i = 0;i<=1;i++){//重複兩次
         System.out.println("Input:");//輸出
         int num1 = scn.nextInt();//輸入變數1
         int num2 = scn.nextInt();//輸入變數2
