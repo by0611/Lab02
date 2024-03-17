93c002c6ab430a16193bf66ef9cda83b29392804
by0611 <156165417+by0611@users.noreply.github.com>
2024-03-17 11:31:11 +0800

    Update JPA04.java

--- JPA04.java
+++ JPA04.java
@@ -1,8 +1,8 @@
-package Lab02;
 import java.util.Scanner;
 public class JPA04 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);//宣告scn為輸入清單
+        for(int i = 0;i <= 1; i++ ){
         System.out.println("Input:");//輸出
         int num = scn.nextInt();//宣告整數變數num
         if (num % 5 == 0 && num % 9 == 0){//這邊需要用到&&(and)來連接兩個不同條件，||是or
@@ -10,7 +10,7 @@ public class JPA04 {
         }
         else{
             System.out.println("No");//條件不成立
-        }
+        }}
         scn.close();
     }
 }
