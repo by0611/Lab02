4440001984e7f3153e79cee29be59a6141d5534e
by0611 <156165417+by0611@users.noreply.github.com>
2024-03-17 11:28:45 +0800

    Update JPA02.java

--- JPA02.java
+++ JPA02.java
@@ -1,8 +1,8 @@
-package Lab02;
 import java.util.Scanner;
 public class JPA02 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);//宣告scn為輸入清單
+        for(int i = 0;i<=1;i++){
         System.out.println("Input:");//輸出
         int num1 = scn.nextInt();//輸入變數1
         int num2 = scn.nextInt();//輸入變數2
@@ -11,7 +11,8 @@ public class JPA02 {
         }
         else{
             System.out.println(num2 +" is large than "+num1);
-        }
+        }}
         scn.close();
     }
 }
+
